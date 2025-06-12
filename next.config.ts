f005d056aad8b1207879e1fc59593a49408a5fc2
vladyslavzinkovskyi <zinkovskyi.vladyslav@gmail.com>
2025-06-12 17:04:54 +0200

    update deployment configuration and next.js settings for optimized builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: "build",
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/t212-analizer" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
